2a00800600adbcbc989c3cee4c8c4fe6cb68cb0b
07jdbouk <07jdbouk@brightoncollege.net>
2021-03-04 00:15:49 +0000

    added new push(), pop(), isFull(), and isEmpty() methods

--- Program.cs
+++ Program.cs
@@ -6,7 +6,59 @@ namespace stack
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            //Console.WriteLine("Hello World!");
+            
+            Stack st = new Stack();
+
+            st.push("pog");
+
+            Console.WriteLine(st.pop());
+        }
+    }
+
+    class Stack 
+    {
+        private string[] stack = new string[8];
+
+        private int front = 8;
+        private int rear = 8;
+
+        public bool push(string item) 
+        {
+            if (!isFull()) {
+                front--;
+                stack[front] = item;
+                return true;
+            }
+
+            return false;
+        }
+
+        public string pop()
+        {
+            if (!isEmpty()) {
+                string item = stack[front];
+                front++;
+                return item;
+            }
+            
+            return null;
+        }
+
+        public bool isFull() 
+        {
+            if (front <= 0)
+                return true;
+            
+            return false;
+        }
+
+        public bool isEmpty() 
+        {
+            if (front >= 8)
+                return true;
+
+            return false;
         }
     }
 }
